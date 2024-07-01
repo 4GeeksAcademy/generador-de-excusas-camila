4cbb1eab302af7bab6e255c6f2732f5445815edb
felixcamila <138735721+felixcamila@users.noreply.github.com>
2024-07-01 19:01:07 +0000

    cambio de js y html

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //aleatorios
+  let aleatorioWho = Math.floor(Math.random() * who.length);
+  let aleatorioAction = Math.floor(Math.random() * action.length);
+  let aleatorioWhat = Math.floor(Math.random() * what.length);
+  let aleatorioWhen = Math.floor(Math.random() * when.length);
+
+  let excuse =
+    who[aleatorioWho] +
+    "  " +
+    action[aleatorioAction] +
+    "  " +
+    what[aleatorioWhat] +
+    "  " +
+    when[aleatorioWhen];
+
+  document.querySelector("#excuse").innerHTML = excuse;
+
   console.log("Hello Rigo from the console!");
 };
