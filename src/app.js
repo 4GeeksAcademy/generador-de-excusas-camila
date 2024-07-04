60ebb417d69bdb28cc18e360b0bff21b0386c81e
felixcamila <138735721+felixcamila@users.noreply.github.com>
2024-07-04 14:10:01 +0000

    refactorización

--- src/app.js
+++ src/app.js
@@ -1,39 +1,29 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+function generateExcuse() {
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "my phone", "the car"];
   let when = [
     "before the class",
-    "when I was sleeping",
+    "while I was sleeping",
     "while I was exercising",
     "during my lunch",
     "while I was praying"
   ];
 
-  //aleatorios
-  let aleatorioWho = Math.floor(Math.random() * who.length);
-  let aleatorioAction = Math.floor(Math.random() * action.length);
-  let aleatorioWhat = Math.floor(Math.random() * what.length);
-  let aleatorioWhen = Math.floor(Math.random() * when.length);
+  let excuse = "";
+  let randomElements = [who, action, what, when];
 
-  let excuse =
-    who[aleatorioWho] +
-    "  " +
-    action[aleatorioAction] +
-    "  " +
-    what[aleatorioWhat] +
-    "  " +
-    when[aleatorioWhen];
+  for (let i = 0; i < randomElements.length; i++) {
+    let randomIndex = Math.floor(Math.random() * randomElements[i].length);
+    excuse += randomElements[i][randomIndex] + " ";
+  }
 
-  document.querySelector("#excuse").innerHTML = excuse;
+  document.querySelector("#excuse").innerHTML = excuse.trim();
+}
 
-  console.log("Hello Rigo from the console!");
-};
+document.addEventListener("DOMContentLoaded", function() {
+  generateExcuse();
+});
